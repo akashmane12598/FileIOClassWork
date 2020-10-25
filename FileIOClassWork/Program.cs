16168fda7041cf78f544560b50b6592b9a11fc6c
Akash <akashmane12598@gmail.com>
2020-10-25 12:53:50 +0530

    [Akash] Add. R/W using File Class

--- FileIOClassWork/Program.cs
+++ FileIOClassWork/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.IO;
 
 namespace FileIOClassWork
 {
     class Program
     {
+        public static string filePath= @"C:\Users\LENOVO\source\repos\FileIOClassWork\FileIOClassWork\File.txt";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to FileIO program!");
+            if (FileExists())
+            {
+                Console.WriteLine("Writing data into a file");
+                FileWrite();
+                Console.WriteLine("Reading data from file");
+                FileRead();
+            }
+        }
+
+        public static bool FileExists()
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("File Exists");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("File Doesn't Exists");
+                return false;
+            }
+        }
+
+        public static void FileWrite()
+        {
+            string content = "Ny Name is Akash."+"\n"+"I am an Engineer.";
+            File.WriteAllText(filePath, content);
+        }
+
+        public static void FileRead()
+        {
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine(content);
         }
     }
 }
